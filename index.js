f0c469f671ceaeb6bada7c08d6f7fc5b92060259
Unknown <verner@fortelius.net>
2018-08-25 00:41:16 +0300

    move all validation components to this module

---
+++ index.js
@@ -0,0 +1,9 @@
+import TextInputValidated from './src/TextInputValidated';
+import TextAreaInputValidated from './src/TextAreaInputValidated';
+import ValidatedForm from './src/ValidatedForm';
+
+export {
+    TextInputValidated,
+    TextAreaInputValidated,
+    ValidatedForm
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ var path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './src/PaginationControl.js',
+    entry: ['babel-polyfill', './index.js'],
     output: {
         path: path.resolve('lib'),
-        filename: 'PaginationControl.js',
+        filename: 'flexinets-react-validation.js',
         libraryTarget: 'commonjs2'
     },
     module: {
@@ -18,8 +18,7 @@ module.exports = {
         ]
     },
     externals: {
-        'react': 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-        'reactstrap': 'reactstrap',
+        'react': 'commonjs react',
         'prop-types': 'prop-types'
     }
 }
