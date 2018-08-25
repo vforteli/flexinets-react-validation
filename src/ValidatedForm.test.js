7cb67de6420276dedf6735adbd55381a480a3e99
Unknown <verner@fortelius.net>
2018-08-25 23:50:53 +0300

    teststuff

--- src/ValidatedForm.test.js
+++ src/ValidatedForm.test.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ValidatedForm from './ValidatedForm';
 import TextInputValidated from './TextInputValidated';
-import renderer from 'react-test-renderer';
 
 test('ValidatedForm renders without errors', () => {
-    const tree = renderer
-        .create(
-            <ValidatedForm>
-                <TextInputValidated type="text" name="test" label="test" required placeholder="test" />
-            </ValidatedForm>
-        ).toJSON();
-    expect(tree).toMatchSnapshot();
+    const wrapper = mount(
+        <ValidatedForm>
+            <TextInputValidated type="text" name="test" label="test" required placeholder="test" />
+        </ValidatedForm>
+    );
+
+    expect(wrapper).toMatchSnapshot();
 });
+
+test('ValidatedForm renders with validation error', () => {
+    const wrapper = mount(
+        <ValidatedForm>
+            <TextInputValidated type="text" name="test" label="test" required placeholder="test" />
+            <button type="submit" id="submitbutton">submit</button>
+        </ValidatedForm>
+    );
+
+    // todo fix this test...
+    wrapper.find('#submitbutton').simulate("click");
+    wrapper.update();
+    expect(wrapper).toMatchSnapshot();
+});
+
 
 // todo add more tests
