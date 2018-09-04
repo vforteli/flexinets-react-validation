775f165b92d1505f1e120fdd445fd3fc1f14ee74
Unknown <verner@fortelius.net>
2018-09-04 19:55:01 +0300

    fix validation feedback

--- src/TextInputAddonValidated.js
+++ src/TextInputAddonValidated.js
@@ -10,13 +10,13 @@ class TextInputAddonValidated extends ValidatedInput {
         return (
             <div className={this.props.required ? 'form-group required' : 'form-group'}>
                 <label htmlFor={this.props.name}>{this.props.label} {!this.props.required && <small>(Optional)</small>}</label>
-                <div className="input-group">
+                <div role="group" className="input-group">
                     <input onBlur={this.checkValidity} className={this.state.hasError ? 'form-control is-invalid' : 'form-control'} {...rest} onChange={this.handleChange} ref={this.inputRef} />
                     <div className="input-group-append">
                         <span className="input-group-text" id="basic-addon2">@{this.props.addonText}</span>
                     </div>
-                </div>
-                <ValidationFeedback validity={this.state.validity} errorMessages={this.state.errorMessages} />
+                    <ValidationFeedback validity={this.state.validity} errorMessages={this.state.errorMessages} />
+                </div>                
                 {this.props.children}
             </div>
         );
